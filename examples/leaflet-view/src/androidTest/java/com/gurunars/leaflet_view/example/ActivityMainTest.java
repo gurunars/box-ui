9f14c770ce82d9c489295d08695c102c172d6c06
Anton Berezin <gurunars@gmail.com>
2017-03-17 17:36:42 +0200

    Updated support deps

--- examples/leaflet-view/src/androidTest/java/com/gurunars/leaflet_view/example/ActivityMainTest.java
+++ examples/leaflet-view/src/androidTest/java/com/gurunars/leaflet_view/example/ActivityMainTest.java
@@ -25,6 +25,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.gurunars.test_utils.Helpers.nthChildOf;
 import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
