d25c4573ed3ee6cde761f908a882808dc4b4f05e
Anton Berezin <gurunars@gmail.com>
2017-06-19 17:44:27 +0300

    ff

--- item-list/src/main/kotlin/com/gurunars/item_list/BindableViewHolder.kt
+++ item-list/src/main/kotlin/com/gurunars/item_list/BindableViewHolder.kt
@@ -1,33 +1,27 @@
+
 package com.gurunars.item_list
 
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
+import com.gurunars.databinding.bindableField
 import com.gurunars.item_list.ItemViewBinderEmpty.Companion.EMPTY_TYPE
 import com.gurunars.shortcuts.asRow
 import com.gurunars.shortcuts.fullSize
 
 
-internal class BindableViewHolder<ViewType : View, ItemType : Item> : RecyclerView.ViewHolder {
-
-    private var itemViewBinder: ItemViewBinder<ViewType, ItemType>? = null
+internal class BindableViewHolder<ItemType : Item> : RecyclerView.ViewHolder {
 
     constructor(root: ViewGroup,
-                itemViewBinder: ItemViewBinder<ViewType, ItemType>) : super(itemViewBinder.getView(root.context)) {
-        this.itemViewBinder = itemViewBinder
-        itemView.asRow()
-    }
+                itemViewBinder: ItemViewBinder<ItemType>) :
+        super(itemViewBinder.getView(root.context, root.bindableField(Pair(null, null))).apply {
+            asRow()
+        })
 
     constructor(root: ViewGroup,
-                emptyViewBinder: EmptyViewBinder) : super(emptyViewBinder.getView(root.context)) {
-        this.itemViewBinder = null
-        itemView.apply {
+                emptyViewBinder: EmptyViewBinder) :
+        super(emptyViewBinder.getView(root.context).apply {
+            fullSize()
             setTag(EMPTY_TYPE, EMPTY_TYPE)
-            this.fullSize()
-        }
-    }
+        })
 
-    fun bind(item: ItemType, previousItem: ItemType?) {
-        itemViewBinder?.bind(itemView as ViewType, item, previousItem)
-    }
 }
--- item-list/src/main/kotlin/com/gurunars/item_list/ItemViewBinder.kt
+++ item-list/src/main/kotlin/com/gurunars/item_list/ItemViewBinder.kt
@@ -2,33 +2,24 @@ package com.gurunars.item_list
 
 import android.content.Context
 import android.view.View
+import com.gurunars.databinding.BindableField
 
 
 /**
  * Glue between the Item of a specific type and its view.
  *
- * @param <ViewType> type of the view to be bound to the item
  * @param <ItemType> type of a payload to be passed to a view
  */
-interface ItemViewBinder<ViewType : View, ItemType : Item> {
+interface ItemViewBinder<ItemType : Item> {
 
     /**
      * Return a view instance to be populated
      *
      * @param context to be used for rendering
+     * @param payload observable of a Pair, where the first element is the current version
+     *                of the item and the second element is a previous version of the item
+     *                if there was any
      * @return rendered view
      */
-    fun getView(context: Context): ViewType
-
-    /**
-     * Populate the view based on data from the item.
-     *
-     * Use previous item to make a decision about which animation to use by diffing it with the
-     * current item.
-     *
-     * @param itemView view to be populated - the one returned by getView
-     * @param item payload to be used for populating the view
-     * @param previousItem previous version of the item. Can be null.
-     */
-    fun bind(itemView: ViewType, item: ItemType, previousItem: ItemType?)
+    fun getView(context: Context, payload: BindableField<Pair<ItemType?, ItemType?>>): View
 }
