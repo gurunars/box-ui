4e5f1aec90a70f04e72920bd1f677514a39b7e37
Anton Berezin <gurunars@gmail.com>
2017-06-21 20:35:04 +0300

    ff

--- examples/crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/example/AnimalItem.kt
+++ examples/crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/example/AnimalItem.kt
@@ -2,9 +2,9 @@ package com.gurunars.crud_item_list.example
 
 import com.gurunars.item_list.Item
 
-internal class AnimalItem(private var id: Long, private var version: Int, private var type: AnimalItem.Type) : Item {
+internal class AnimalItem(private val id: Long, private var version: Int, private val type: AnimalItem.Type) : Item {
 
-    override fun getType(): AnimalItem.Type {
+    override fun getType(): Enum<*> {
         return type
     }
 
@@ -17,16 +17,14 @@ internal class AnimalItem(private var id: Long, private var version: Int, privat
     }
 
     internal enum class Type {
-        MONKEY, TIGER, WOLF, LION
+        MONKEY, TIGER, WOLF, LION, EMPTY
     }
 
     fun update() {
         this.version++
     }
 
-    fun getVersion(): Int {
-        return version
-    }
+    fun getVersion() = version
 
     override fun toString(): String {
         return "#$id{$type @ $version}"
