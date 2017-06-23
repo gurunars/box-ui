215feab7143a1cbdf3dae209f00e5a9b7a9b28fd
Anton Berezin <gurunars@gmail.com>
2017-06-24 00:11:11 +0300

    Cleanup

--- android-utils/src/main/kotlin/com/gurunars/android_utils/Anko.kt
+++ android-utils/src/main/kotlin/com/gurunars/android_utils/Anko.kt
@@ -3,5 +3,4 @@ package com.gurunars.android_utils
 import android.view.ViewManager
 import org.jetbrains.anko.custom.ankoView
 
-fun ViewManager.iconView(theme: Int = 0) = iconView(theme) {}
-fun ViewManager.iconView(theme: Int = 0, init: IconView.() -> Unit) = ankoView({ IconView(it) }, theme, init)
+fun ViewManager.iconView(init: IconView.() -> Unit) = ankoView({ IconView(it) }, 0, init)
