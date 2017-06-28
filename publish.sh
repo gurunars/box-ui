3d0cd2091ee7d33fab0454fe9559df8485297cf8
Anton Berezin <gurunars@gmail.com>
2017-06-28 14:53:29 +0300

    Docs completed

--- publish.sh
+++ publish.sh
@@ -4,17 +4,12 @@ set -eu
 
 GITHUB_ACCOUNT=gurunars
 PROJECT_NAME=android-crud-ui
-declare -a PUBLIC_MODULES=("android-utils" "floatmenu" "item-list" "leaflet-view" "crud-item-list")
 
 TMP=/tmp/pages.${PROJECT_NAME}
 
-./gradlew test connectedAndroidTest generateReleaseJavadoc bintrayUpload
+./gradlew --rerun-tasks test connectedAndroidTest dokka bintrayUpload
 rm -rf ${TMP}
-mkdir ${TMP}
-
-for module in "${PUBLIC_MODULES[@]}"; do
-    mv ${module}/build/docs/javadoc ${TMP}/${module}
-done
+mv html-docs ${TMP}
 
 cd ${TMP}
 git init
