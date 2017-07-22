7d90ad9f825a3111ec56833d89e1f31d83b00ef8
Anton Berezin <gurunars@gmail.com>
2017-07-22 19:49:46 +0300

    ff

--- floatmenu/src/main/kotlin/com/gurunars/floatmenu/FloatMenu.kt
+++ floatmenu/src/main/kotlin/com/gurunars/floatmenu/FloatMenu.kt
@@ -13,20 +13,29 @@ import com.gurunars.shortcuts.setOneView
 import org.jetbrains.anko.*
 
 /**
- * Floating menu available via a [FAB](https://material.google.com/components/buttons-floating-action-button.html)
+ * Floating menu available via a
+ * [FAB](https://material.google.com/components/buttons-floating-action-button.html)
  *
- * @property isLeftHanded If **true** - is on the left side of the screen. On the right side otherwise.
- * @property animationDuration Time it takes to perform all the animated UI transitions.
- * @property isOpen If **true** contents are visible. Menu contents are hidden otherwise.
- * @property openIcon Icon associated with the open state of the menu. Shown when the menu is closed.
- * @property closeIcon Icon associated with the closed state of the menu. Show when the menu is open.
- * @property hasOverlay If **true** the menu has a shaded background that intercepts clicks. If **false** - the menu
- *                      does not intercept clicks and passes them to the content area. The flag does not affect
- *                      clickable elements that are located inside the menu though.
- * @property contentView View shown in the background layer of the widget. Semantically it represents the data
- *                       manipulated by the menu.
- * @property menuView View shown in the foreground layer of the widget when the menu is open. Is supposed to
- *                    contain menu's controls.
+ * @property isLeftHanded
+ *     If **true** - is on the left side of the screen. On the right side otherwise.
+ * @property animationDuration
+ *     Time it takes to perform all the animated UI transitions.
+ * @property isOpen
+ *     If **true** contents are visible. Menu contents are hidden otherwise.
+ * @property openIcon
+ *     Icon associated with the open state of the menu. Shown when the menu is closed.
+ * @property closeIcon
+ *     Icon associated with the closed state of the menu. Show when the menu is open.
+ * @property hasOverlay
+ *     If **true** the menu has a shaded background that intercepts clicks. If **false** - the menu
+ *     does not intercept clicks and passes them to the content area. The flag does not affect
+ *     clickable elements that are located inside the menu though.
+ * @property contentView
+ *     View shown in the background layer of the widget. Semantically it represents the data
+ *     manipulated by the menu.
+ * @property menuView
+ *     View shown in the foreground layer of the widget when the menu is open. Is supposed to
+ *     contain menu's controls.
  */
 class FloatMenu constructor(context: Context) : FrameLayout(context) {
     val isLeftHanded = bindableField(false)
