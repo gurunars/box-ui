a30a11692b45bdb17087191f41045931dd83eac3
Anton Berezin <gurunars@gmail.com>
2017-07-22 12:32:36 +0300

    ff

--- databinding/src/main/kotlin/com/gurunars/databinding/views/TextView.kt
+++
@@ -1,9 +0,0 @@
-package com.gurunars.databinding.views
-
-import android.widget.TextView
-import com.gurunars.databinding.BindableField
-
-
-inline fun BindableField<String>.bind(textView: TextView) {
-    onChange { textView.setText(it) }
-}
