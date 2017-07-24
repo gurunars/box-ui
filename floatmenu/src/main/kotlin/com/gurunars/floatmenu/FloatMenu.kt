e320778e33d2dd0af12b3058cb88873eae09d7e5
Anton Berezin <gurunars@gmail.com>
2017-07-24 22:22:36 +0300

    ff

--- floatmenu/src/main/kotlin/com/gurunars/floatmenu/FloatMenu.kt
+++ floatmenu/src/main/kotlin/com/gurunars/floatmenu/FloatMenu.kt
@@ -83,6 +83,7 @@ class FloatMenu constructor(context: Context) : FrameLayout(context) {
         return Bundle().apply {
             putParcelable("superState", super.onSaveInstanceState())
             putBoolean("isOpen", isOpen.get())
+            putBoolean("isLeftHanded", isLeftHanded.get())
         }
     }
 
@@ -93,6 +94,7 @@ class FloatMenu constructor(context: Context) : FrameLayout(context) {
         val localState = state as Bundle
         super.onRestoreInstanceState(localState.getParcelable("superState"))
         isOpen.set(localState.getBoolean("isOpen"))
+        isLeftHanded.set(localState.getBoolean("isLeftHanded"))
     }
 
 }
