34685490405544a340ea1dc1a2ba7c596d17daff
Anton Berezin <gurunars@gmail.com>
2017-08-09 23:48:57 +0300

    Added two more android data bindings

--- databinding/src/main/kotlin/com/gurunars/databinding/android/CompoundButton.kt
+++ databinding/src/main/kotlin/com/gurunars/databinding/android/CompoundButton.kt
@@ -12,8 +12,31 @@ fun CompoundButton.bind(field: BindableField<Boolean>) {
 }
 
 /**
+ * Same as regular bind but with two way value transformation.
+ *
+ * @param From - type of payload to be transformed into a boolean and to be generated out of a boolean
+ */
+fun<From> CompoundButton.bind(
+    field: BindableField<From>,
+    valueTransformer: BindableField.ValueTransformer<From, Boolean>
+) {
+    field.onChange { isChecked=valueTransformer.forward(it) }
+    setOnCheckedChangeListener { _, isChecked -> field.set(valueTransformer.backward(isChecked)) }
+}
+
+/**
  * @see CompoundButton.bind
  */
 fun BindableField<Boolean>.bind(compoundButton: CompoundButton) {
     compoundButton.bind(this)
+}
+
+/**
+ * @see CompoundButton.bind with value transformation
+ */
+fun<From> BindableField<From>.bind(
+    compoundButton: CompoundButton,
+    valueTransformer: BindableField.ValueTransformer<From, Boolean>
+) {
+    compoundButton.bind(this, valueTransformer)
 }
--- databinding/src/main/kotlin/com/gurunars/databinding/android/EditText.kt
+++ databinding/src/main/kotlin/com/gurunars/databinding/android/EditText.kt
@@ -2,12 +2,11 @@ package com.gurunars.databinding.android
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.EditText
 import android.widget.TextView
 import com.gurunars.databinding.BindableField
 
 /**
- * Ensure that a string field is changed whenever CompoundButton's value changes and vice versa.
+ * Ensure that a string field is changed whenever TextView's value changes and vice versa.
  */
 fun TextView.bind(field: BindableField<String>) {
     field.onChange { setText(it) }
@@ -19,13 +18,44 @@ fun TextView.bind(field: BindableField<String>) {
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             field.set(s.toString())
         }
-
     })
 }
+
+/**
+ * Same as regular bind but with two way value transformation.
+ *
+ * @param From - type of payload to be transformed into a string and to be generated out of a string
+ */
+fun<From> TextView.bind(
+    field: BindableField<From>,
+    valueTransformer: BindableField.ValueTransformer<From, String>
+) {
+    field.onChange { setText(valueTransformer.forward(it)) }
+    addTextChangedListener(object: TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            field.set(valueTransformer.backward(s.toString()))
+        }
+    })
+}
+
 
 /**
  * @see TextView.bind
  */
 fun BindableField<String>.bind(textView: TextView) {
     textView.bind(this)
+}
+
+/**
+ * @see TextView.bind with value transformation
+ */
+fun<From> BindableField<From>.bind(
+    textView: TextView,
+    valueTransformer: BindableField.ValueTransformer<From, String>
+) {
+    textView.bind(this, valueTransformer)
 }
