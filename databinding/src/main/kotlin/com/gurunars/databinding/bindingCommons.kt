b137974d5b923d5e5c4d72c22382d3a66aeb6adb
Anton Berezin <gurunars@gmail.com>
2017-10-11 21:28:07 +0300

    childField instead of map for clarity

--- databinding/src/main/kotlin/com/gurunars/databinding/bindingCommons.kt
+++ databinding/src/main/kotlin/com/gurunars/databinding/bindingCommons.kt
@@ -1,23 +1,23 @@
 package com.gurunars.databinding
 
-fun <From, To> BindableField<From>.map(
+fun <From, To> BindableField<From>.childField(
     reduce: From.() -> To
 ) = BindableField(get().reduce()).apply {
-    this@map.onChange {
-        set(this@map.get().reduce())
+    this@childField.onChange {
+        set(this@childField.get().reduce())
     }
 }
 
 fun<ItemType> BindableField<ItemType>.patch(patcher: ItemType.() -> ItemType) =
     set(get().patcher())
 
-fun <From, To> BindableField<From>.map(
+fun <From, To> BindableField<From>.childField(
     reduce: From.() -> To,
-    patch: From.(part: To) -> From
+    patchParent: From.(part: To) -> From
 ) = BindableField(get().reduce()).apply {
-    this@map.bind(
+    this@childField.bind(
         this,
         { it.reduce() },
-        { this@map.get().patch(it) }
+        { this@childField.get().patchParent(it) }
     )
 }
