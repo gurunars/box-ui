673d7c765403c28fd732fa5dcb81f160257a53d4
Anton Berezin <gurunars@gmail.com>
2017-11-04 11:50:58 +0200

    Fixed publisher script

--- publish.sh
+++ publish.sh
@@ -4,12 +4,11 @@ set -eu
 
 GITHUB_ACCOUNT=gurunars
 PROJECT_NAME=android-crud-ui
+DOMAIN_NAME=android-crud-ui.gurunars.com
 
 TMP=/tmp/pages.${PROJECT_NAME}
 
 ./gradlew --rerun-tasks dokka bintrayUpload
-
-git push origin master --tags
 
 rm -rf ${TMP}
 mv html-docs ${TMP}
@@ -17,7 +16,7 @@ mv html-docs ${TMP}
 cd ${TMP}
 git init
 git checkout -b gh-pages
-echo "android-crud-ui.gurunars.com" > CNAME
+echo "${DOMAIN_NAME}" > CNAME
 git add .
 git commit -am init
 git remote add origin git@github.com:${GITHUB_ACCOUNT}/${PROJECT_NAME}.git
