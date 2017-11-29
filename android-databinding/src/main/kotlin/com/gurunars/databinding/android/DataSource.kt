9dc644a21f9abeedc578d8130c7fccb02679777e
Anton Berezin <gurunars@gmail.com>
2017-11-30 00:00:56 +0200

    Working DataStore

--- android-databinding/src/main/kotlin/com/gurunars/databinding/android/DataSource.kt
+++ android-databinding/src/main/kotlin/com/gurunars/databinding/android/DataSource.kt
@@ -6,17 +6,26 @@ import com.gurunars.databinding.Listener
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
+/**
+ * Special IBox to glue data storage with an Observerable value
+ * in asynchronous manner.
+ *
+ * @param getF reads data from the storage
+ * @param setF writes data to the storage
+ * @param preprocess alters the payload before storing it
+ * @param initial temporary payload to be
+ */
 class DataSource<Type>(
     private val getF: () -> Type,
     private val setF: (value: Type) -> Any,
-    private val preset: (value: Type) -> Type = { it },
+    private val preprocess: (value: Type) -> Type = { it },
     initial: Type
 ) : IBox<Type> {
     private val box = Box(initial)
 
     fun refetch() {
         doAsync {
-            val next = preset(getF())
+            val next = preprocess(getF())
             uiThread {
                 box.set(next, true)
             }
@@ -30,7 +39,7 @@ class DataSource<Type>(
     override fun get() = box.get()
 
     override fun set(value: Type, force: Boolean): Boolean {
-        val sorted = preset(value)
+        val sorted = preprocess(value)
         if (box.set(sorted, force)) {
             doAsync {
                 setF(sorted)
