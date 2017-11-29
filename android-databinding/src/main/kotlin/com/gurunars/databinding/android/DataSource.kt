f260e2dfbc97130db89fcb3c5dfb287fea08dc4a
Anton Berezin <gurunars@gmail.com>
2017-11-29 23:06:30 +0200

    Fixed the data source bug

--- android-databinding/src/main/kotlin/com/gurunars/databinding/android/DataSource.kt
+++ android-databinding/src/main/kotlin/com/gurunars/databinding/android/DataSource.kt
@@ -9,25 +9,31 @@ import org.jetbrains.anko.uiThread
 class DataSource<Type>(
     private val getF: () -> Type,
     private val setF: (value: Type) -> Any,
+    private val preset: (value: Type) -> Type = { it },
     initial: Type
 ) : IBox<Type> {
     private val box = Box(initial)
 
     fun refetch() {
         doAsync {
-            val next = getF()
+            val next = preset(getF())
             uiThread {
                 box.set(next, false)
             }
         }
     }
 
+    init {
+        refetch()
+    }
+
     override fun get() = box.get()
 
     override fun set(value: Type, force: Boolean): Boolean {
-        if (box.set(value, force)) {
+        val sorted = preset(value)
+        if (box.set(sorted, force)) {
             doAsync {
-                setF(value)
+                setF(sorted)
                 refetch()
             }
             return true
