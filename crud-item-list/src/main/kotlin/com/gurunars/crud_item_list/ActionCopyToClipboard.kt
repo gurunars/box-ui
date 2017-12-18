09165408dc79ef0af8b711f53a96bd5ebfbc227b
Anton Berezin <gurunars@gmail.com>
2017-12-18 17:32:25 +0200

    Maded toasts less annoying

--- crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/ActionCopyToClipboard.kt
+++ crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/ActionCopyToClipboard.kt
@@ -3,9 +3,9 @@ package com.gurunars.crud_item_list
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.widget.Toast
 import com.gurunars.item_list.Item
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.longToast
 import org.jetbrains.anko.uiThread
 
 internal class ActionCopyToClipboard<ItemType : Item>(
@@ -15,9 +15,11 @@ internal class ActionCopyToClipboard<ItemType : Item>(
 
     private val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
+    private val toast = Toast.makeText(context, R.string.copiedToClipboard, Toast.LENGTH_LONG)
+
     private fun writeToClipboard(label: String, payload: String) {
         clipboard.primaryClip = ClipData.newPlainText(label, payload)
-        context.longToast(R.string.copiedToClipboard)
+        toast.show()
     }
 
     override fun perform(
--- crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/ActionPasteFromClipboard.kt
+++ crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/ActionPasteFromClipboard.kt
@@ -2,9 +2,9 @@ package com.gurunars.crud_item_list
 
 import android.content.ClipboardManager
 import android.content.Context
+import android.widget.Toast
 import com.gurunars.item_list.Item
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.longToast
 import org.jetbrains.anko.uiThread
 
 internal class ActionPasteFromClipboard<ItemType : Item>(
@@ -13,11 +13,17 @@ internal class ActionPasteFromClipboard<ItemType : Item>(
 ) : Action<ItemType> {
 
     private val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    private val okToast = Toast.makeText(context, R.string.pastedFromClipboard, Toast.LENGTH_LONG)
+    private val nokToast = Toast.makeText(context, R.string.failedToPaste, Toast.LENGTH_LONG)
 
     private fun getPasteCandidates(): List<ItemType> {
-        if (!clipboard.hasPrimaryClip()) { return listOf() }
+        if (!clipboard.hasPrimaryClip()) {
+            return listOf()
+        }
         val clip = clipboard.primaryClip
-        if (clip.itemCount <= 0) { return listOf() }
+        if (clip.itemCount <= 0) {
+            return listOf()
+        }
         return try {
             serializer.fromString(clip.getItemAt(0).text.toString())
         } catch (exe: Exception) {
@@ -34,10 +40,10 @@ internal class ActionPasteFromClipboard<ItemType : Item>(
             val full = getPasteCandidates()
             uiThread {
                 if (full.isNotEmpty()) {
-                    context.longToast(R.string.pastedFromClipboard)
+                    okToast
                 } else {
-                    context.longToast(R.string.failedToPaste)
-                }
+                    nokToast
+                }.show()
                 consumer(all + full, selectedItems)
             }
         }
