fb5c5da3f5ceaabb941bdbd408a9047fbdc9d250
Anton Berezin <gurunars@gmail.com>
2018-01-13 01:42:39 +0200

    ff

---
+++ examples/storybook/src/main/kotlin/com/gurunars/storybook/example/ActivityMain.kt
@@ -0,0 +1,13 @@
+package com.gurunars.storybook.example
+
+import android.app.Activity
+import android.os.Bundle
+
+class ActivityMain : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
