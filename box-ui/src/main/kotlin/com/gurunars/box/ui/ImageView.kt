d01792c6da7e5c2201262d4d8e7132d8dc738260
Anton Berezin <gurunars@gmail.com>
2018-01-14 03:31:50 +0200

    Use IRoBox in image src

--- box-ui/src/main/kotlin/com/gurunars/box/ui/ImageView.kt
+++ box-ui/src/main/kotlin/com/gurunars/box/ui/ImageView.kt
@@ -1,7 +1,7 @@
 package com.gurunars.box.ui
 
 import android.widget.ImageView
-import com.gurunars.box.IBox
+import com.gurunars.box.IRoBox
 
-fun ImageView.src(field: IBox<Int>) =
+fun ImageView.src(field: IRoBox<Int>) =
     field.onChange(listener = this::setImageResource)
