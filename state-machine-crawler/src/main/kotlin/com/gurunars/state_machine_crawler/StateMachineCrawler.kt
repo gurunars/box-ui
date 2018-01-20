1d15e2e4b4ca70f546fe1832fea6c980d701b816
Anton Berezin <anton.berezin@nephilim.solutions>
2018-01-20 22:45:47 +0200

    Added state machine crawler

---
+++ state-machine-crawler/src/main/kotlin/com/gurunars/state_machine_crawler/StateMachineCrawler.kt
@@ -0,0 +1,4 @@
+package com.gurunars.state_machine_crawler
+
+interface StateMachineCrawler {
+}
---
+++ state-machine-crawler/src/test/kotlin/com/gurunars/box/StateMachineCrawlerTest.kt
@@ -0,0 +1,12 @@
+package com.gurunars.state_machine_crawler
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class StateMachineCrawlerTest {
+
+    @Test
+    fun onChange() {
+    }
+
+}
