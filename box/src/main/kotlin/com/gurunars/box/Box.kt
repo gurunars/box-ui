c5d11a5c396cf25a10c035135d81514c6534ac1d
Anton Berezin <anton.berezin@nephilim.solutions>
2018-02-15 10:02:03 +0200

    locking mutable box operations

--- box/src/main/kotlin/com/gurunars/box/Box.kt
+++ box/src/main/kotlin/com/gurunars/box/Box.kt
@@ -1,6 +1,6 @@
 package com.gurunars.box
 
-import java.util.Objects
+import java.util.*
 
 /**
  * An observable box capable to emit changes and listen to change events
@@ -12,27 +12,29 @@ class Box<Type>(private var value: Type) : IBox<Type> {
     private val listeners: MutableList<(value: Type) -> Unit> = mutableListOf()
 
     override fun onChange(hot: Boolean, listener: (value: Type) -> Unit): Bond {
-        listeners.add(listener)
-        if (hot) listener(this.value)
+        synchronized(this) {
+            listeners.add(listener)
+            if (hot) listener(this.value)
 
-        return object : Bond {
-            override fun drop() {
-                listeners.remove(listener)
+            return object : Bond {
+                override fun drop() {
+                    synchronized(this) {
+                        listeners.remove(listener)
+                    }
+                }
             }
         }
     }
 
-    private fun notifyListeners() = listeners.forEach {
-        it.invoke(this.value)
-    }
-
     override fun set(value: Type, force: Boolean): Boolean {
-        if (force || !Objects.deepEquals(this.value, value)) {
-            this.value = value
-            notifyListeners()
-            return true
+        synchronized(this) {
+            if (force || !Objects.deepEquals(this.value, value)) {
+                this.value = value
+                listeners.forEach { it.invoke(value) }
+                return true
+            }
+            return false
         }
-        return false
     }
 
     override fun get(): Type = this.value
