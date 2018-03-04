cb971f4969d0e5588ee1c3aa69d81384a0a2067b
Anton Berezin <anton.berezin@nephilim.solutions>
2018-03-04 23:59:03 +0200

    Resolved android room issue

--- examples/animal-item/src/main/kotlin/com/gurunars/animal_item/AnimalItem.kt
+++ examples/animal-item/src/main/kotlin/com/gurunars/animal_item/AnimalItem.kt
@@ -1,16 +1,19 @@
 package com.gurunars.animal_item
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
 
 @Entity
-data class AnimalItem(
+data class AnimalItem @Ignore constructor(
     @PrimaryKey(autoGenerate = true)
     override var id: Long = 0,
     override var type: AnimalItem.Type = Type.EMPTY,
     override var position: Int = 0,
     var version: Int = 0
 ) : ItemWithPosition {
+
+    constructor(): this(0)
 
     enum class Type(val resourceId: Int) {
         MONKEY(R.drawable.ic_menu_monkey), TIGER(R.drawable.ic_menu_tiger),
