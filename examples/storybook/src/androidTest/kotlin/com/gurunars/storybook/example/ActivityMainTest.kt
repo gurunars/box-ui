b420186604098373623c80fe10405503a541d46b
Anton Berezin <anton.berezin@nephilim.solutions>
2018-05-15 01:45:10 +0300

    Fixed storybook tests

--- examples/storybook/src/androidTest/kotlin/com/gurunars/storybook/example/ActivityMainTest.kt
+++ examples/storybook/src/androidTest/kotlin/com/gurunars/storybook/example/ActivityMainTest.kt
@@ -6,6 +6,7 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.LargeTest
 import android.support.test.runner.AndroidJUnit4
+import com.gurunars.storybook.ActivityStorybook
 import com.gurunars.test_utils.DebugActivityRule
 import org.junit.Rule
 import org.junit.Test
