0ddc1c8de06dfbd77abe6e2e7226f1b44ad28647
Anton Berezin <anton.berezin@nephilim.solutions>
2018-05-15 00:19:48 +0300

    Removed odd import

--- examples/selectable-item-list/src/androidTest/kotlin/com/gurunars/item_list/selectable_example/ActivityMainTest.kt
+++ examples/selectable-item-list/src/androidTest/kotlin/com/gurunars/item_list/selectable_example/ActivityMainTest.kt
@@ -8,7 +8,6 @@ import android.support.test.espresso.action.ViewActions.longClick
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.LargeTest
-import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
 import android.view.View
