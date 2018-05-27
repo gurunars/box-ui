1eda148b0c509ce04bc60354f02f6a3b33242d2b
Anton Berezin <anton.berezin@nephilim.solutions>
2018-05-27 16:40:00 +0300

    ff

--- crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/Action.kt
+++ crud-item-list/src/main/kotlin/com/gurunars/crud_item_list/Action.kt
@@ -3,8 +3,10 @@ package com.gurunars.crud_item_list
 import com.gurunars.box.IRoBox
 import com.gurunars.item_list.Item
 
-internal typealias ItemSetChange<ItemType> =
-    (newAll: List<ItemType>, newSelectedItems: Set<ItemType>) -> Unit
+internal typealias ItemSetChange<ItemType> = (
+    newAll: List<ItemType>,
+    newSelectedItems: Set<ItemType>
+) -> Unit
 
 internal interface Action<ItemType : Item> {
     fun perform(
