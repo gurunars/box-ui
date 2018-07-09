a9cfcf3facb0f739cc1ffd6b90da27708a98ce48
Anton Berezin <anton.berezin@nephilim.solutions>
2018-07-10 00:07:33 +0300

    ff

--- box-ui/src/main/kotlin/com/gurunars/box/ui/component/text/Text.kt
+++ box-ui/src/main/kotlin/com/gurunars/box/ui/component/text/Text.kt
@@ -1,47 +1,19 @@
 package com.gurunars.box.ui.component.text
 
 import android.content.Context
-import android.graphics.Paint
-import android.graphics.Typeface
 import android.widget.TextView
 import com.gurunars.box.ui.component.*
 
 
 data class Text(
-    //Controls whether links such as urls and email addresses are automatically found and converted to clickable links.
-    //val autoLink: Boolean = false,
-    val textAppearence: TextAppearence = TextAppearence(),
+    override val textAppearence: TextAppearence = TextAppearence(),
     val value: String = ""
-)
+): WithTextAppearence
 
 class TextComponent : Component {
     private val changeSpec = listOf<ChangeSpec<Text, *, TextView>>(
-        { it: Text -> it.value } rendersTo  { text = it },
-        { it: Text -> it.textAppearence.color } rendersTo { setTextColor(it.value) },
-        { it: Text -> it.textAppearence.gravity } rendersTo { gravity = it.value },
-        { it: Text -> it.textAppearence.size } rendersTo { textSize = context.toInt(it).toFloat() },
-        { it: Text -> it.textAppearence.textStyle } rendersTo {
-            when {
-                it.isEmpty()
-                    -> setTypeface(typeface, Typeface.NORMAL)
-                it.containsAll(setOf(TextAppearence.TextStyle.BOLD, TextAppearence.TextStyle.ITALIC))
-                    -> setTypeface(typeface, Typeface.BOLD_ITALIC)
-                it.contains(TextAppearence.TextStyle.BOLD)
-                    -> setTypeface(typeface, Typeface.BOLD)
-                it.contains(TextAppearence.TextStyle.ITALIC)
-                    -> setTypeface(typeface, Typeface.ITALIC)
-            }
-
-            paintFlags = when {
-                it.contains(TextAppearence.TextStyle.UNDERLINE)
-                    -> paintFlags or Paint.UNDERLINE_TEXT_FLAG
-                it.contains(TextAppearence.TextStyle.STRIKE_THROUGH)
-                    -> paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                else
-                    -> paintFlags
-            }
-        }
-    )
+        { it: Text -> it.value } rendersTo  { text = it }
+    ) + textAppearenceChangeSpec()
 
     override val empty = Text()
     override fun getEmptyView(context: Context) = TextView(context).apply {
--- box-ui/src/main/kotlin/com/gurunars/box/ui/component/text/TextAppearence.kt
+++ box-ui/src/main/kotlin/com/gurunars/box/ui/component/text/TextAppearence.kt
@@ -1,9 +1,9 @@
 package com.gurunars.box.ui.component.text
 
-import com.gurunars.box.ui.component.Color
-import com.gurunars.box.ui.component.Size
-import com.gurunars.box.ui.component.Gravity
-import com.gurunars.box.ui.component.sp
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.widget.TextView
+import com.gurunars.box.ui.component.*
 
 data class TextAppearence(
     val textStyle: Set<TextStyle> = setOf(),
@@ -15,3 +15,34 @@ data class TextAppearence(
         BOLD, ITALIC, UNDERLINE, STRIKE_THROUGH
     }
 }
+
+interface WithTextAppearence {
+    val textAppearence: TextAppearence
+}
+
+fun<T: WithTextAppearence> textAppearenceChangeSpec() = listOf<ChangeSpec<T, *, TextView>>(
+    { it: T -> it.textAppearence.color } rendersTo { setTextColor(it.value) },
+    { it: T -> it.textAppearence.gravity } rendersTo { gravity = it.value },
+    { it: T -> it.textAppearence.size } rendersTo { textSize = context.toInt(it).toFloat() },
+    { it: T -> it.textAppearence.textStyle } rendersTo {
+        when {
+            it.isEmpty()
+            -> setTypeface(typeface, Typeface.NORMAL)
+            it.containsAll(setOf(TextAppearence.TextStyle.BOLD, TextAppearence.TextStyle.ITALIC))
+            -> setTypeface(typeface, Typeface.BOLD_ITALIC)
+            it.contains(TextAppearence.TextStyle.BOLD)
+            -> setTypeface(typeface, Typeface.BOLD)
+            it.contains(TextAppearence.TextStyle.ITALIC)
+            -> setTypeface(typeface, Typeface.ITALIC)
+        }
+
+        paintFlags = when {
+            it.contains(TextAppearence.TextStyle.UNDERLINE)
+            -> paintFlags or Paint.UNDERLINE_TEXT_FLAG
+            it.contains(TextAppearence.TextStyle.STRIKE_THROUGH)
+            -> paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            else
+            -> paintFlags
+        }
+    }
+)
