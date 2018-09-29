eac21538b5eb4066e95f65a0f394d403c48aa909
Anton Berezin <anton.berezin@nephilim.solutions>
2018-09-29 15:52:12 +0300

    ff

--- functional/src/main/kotlin/com/gurunars/functional/Size.kt
+++ functional/src/main/kotlin/com/gurunars/functional/Size.kt
@@ -9,9 +9,7 @@ object MatchParent : Size()
 object WrapContent : Size()
 data class dp(val size: Int) : Size()
 data class sp(val size: Int) : Size()
-data class pt(val size: Int) : Size()
 data class px(val size: Int) : Size()
-data class mm(val size: Int) : Size()
 
 val Int.dp
     get() = dp(this)
@@ -19,24 +17,15 @@ val Int.dp
 val Int.sp
     get() = sp(this)
 
-val Int.pt
-    get() = pt(this)
-
 val Int.px
     get() = px(this)
-
-val Int.mm
-    get() = mm(this)
-
 
 fun Context.toInt(size: Size): Int =
     when (size) {
         is MatchParent -> MATCH_PARENT
         is WrapContent -> WRAP_CONTENT
-        is dp -> 0
-        is sp -> 0
-        is pt -> 0
-        is px -> 0
-        is mm -> 0
+        is dp -> (size.size * resources.displayMetrics.density).toInt()
+        is sp -> (size.size * resources.displayMetrics.scaledDensity).toInt()
+        is px -> size.size
     }
 
