5fd1850648c9f6a5884eeef4b189a69afdf0ca13
Anton Berezin <gurunars@gmail.com>
2019-02-14 02:55:21 +0200

    ff

--- examples/app/build.gradle.kts
+++ examples/app/build.gradle.kts
@@ -4,26 +4,28 @@ plugins {
     id("kotlin-android-extensions")
 }
 
-
-android {
-    compileSdkVersion(28)
-    defaultConfig {
-        applicationId = "com.example.box"
-        minSdkVersion(23)
-        targetSdkVersion(28)
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+fun Project.stuff() {
+    android {
+        compileSdkVersion(28)
+        defaultConfig {
+            applicationId = "com.example.box"
+            minSdkVersion(23)
+            targetSdkVersion(28)
+            versionCode = 1
+            versionName = "1.0"
+            testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        }
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            }
         }
     }
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
+        implementation("com.android.support:appcompat-v7:28.0.0")
+    }
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
-    implementation("com.android.support:appcompat-v7:28.0.0")
-}
+stuff()
